c49ad1ccffb596f1b316142454afc1643c522f27
kolynzb <collinsbenda360@gmail.com>
2022-06-24 07:47:37 +0300

    Create Custom button

---
+++ tester_app/lib/fancy_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class FancyBtn extends StatelessWidget {
+  final String btnText;
+  final Color btnColor;
+  final GestureTapCallback onPress;
+  const FancyBtn(
+      {Key? key,
+      required this.btnText,
+      this.btnColor = Colors.deepOrangeAccent,
+      required this.onPress})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      fillColor: btnColor,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          btnText.toUpperCase(),
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+      onPressed: onPress,
+      shape: const StadiumBorder(),
+    );
+  }
+}
--- tester_app/lib/main.dart
+++ tester_app/lib/main.dart
@@ -1,1 +1,36 @@
-1:47:34
+import 'package:flutter/material.dart';
+import 'package:tester_app/fancy_button.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "Whats your name",
+      "Whats your fav dish",
+      "Whats your fav dish",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My first app'),
+        ),
+        body: Column(
+          children:  <Widget>[
+            Text(questions[0]),
+            const ElevatedButton(onPressed: answerQuestion, child: Text('Yes')),
+            const ElevatedButton(onPressed: answerQuestion, child: Text('no')),
+            const ElevatedButton(onPressed: null, child: Text('Yes')),
+            FancyBtn(btnText: "Hey", onPress: onPress)
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void answerQuestion() {}
